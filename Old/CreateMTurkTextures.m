541d735008737cfd2f52654e9a0b78946a06ab75
MrPasquino <stuartjduffield@gmail.com>
2021-11-20 09:32:01 -0500

    Updated old mturk textures

--- Old/CreateMTurkTextures.m
+++ Old/CreateMTurkTextures.m
@@ -1,8 +1,8 @@
 %% Set Up Directory Structure
 curDir = pwd; %also circle directory
 CIRCdir = fullfile(curDir,'circle')
-ACHdir = fullfile(curDir,'achromatic');
-CHdir = fullfile(curDir,'chromatic');
+ACHdir = fullfile(curDir,'achromatic','old');
+CHdir = fullfile(curDir,'chromatic','old');
 
 %% Prepare images
 imageNumbers = 14;
@@ -18,14 +18,14 @@ alphaCircle = 255-alphaCircle;
 %% Color Params
 file_colors= round(csvread('/Users/duffieldsj/Documents/GitHub/Personal-Scripts/woosterNIFRGB.csv'))
 file_colors(file_colors>255) = 255;
-cRED = file_colors(2:15,1)'
-cGREEN = file_colors(2:15,2)'
-cBLUE = file_colors(2:15,3)'
-backgroundRGB = file_colors(1,:)
-% cRED = [246, 142, 209, 121, 142, 81, 83, 48, 162, 93, 236, 136, 191, 110];
-% cGREEN = [134, 77, 165, 96, 187, 108, 190, 109, 163, 94, 125, 73, 165, 94];
-% cBLUE = [175, 97, 95, 52, 129, 71, 218, 120, 255, 153, 255, 142, 198, 109];
-% backgroundRGB = [152, 132, 151];
+% cRED = file_colors(2:15,1)'
+% cGREEN = file_colors(2:15,2)'
+% cBLUE = file_colors(2:15,3)'
+% backgroundRGB = file_colors(1,:)
+cRED = [246, 142, 209, 121, 142, 81, 83, 48, 162, 93, 236, 136, 191, 110];
+cGREEN = [134, 77, 165, 96, 187, 108, 190, 109, 163, 94, 125, 73, 165, 94];
+cBLUE = [175, 97, 95, 52, 129, 71, 218, 120, 255, 153, 255, 142, 198, 109];
+backgroundRGB = [152, 132, 151];
 
 
 %% Create Colored Images
@@ -33,7 +33,7 @@ background = cat(3,uint8(zeros(size(alphaCircle))+backgroundRGB(1)),uint8(zeros(
 
 for i = 1:imageNumbers
     
-    chrom(:,:,:,i) = imoverlay(imgCH{i},maskCH{i},[cRED(i),cBLUE(i),cGREEN(i)]/255);
+    chrom(:,:,:,i) = imoverlay(imgCH{i},maskCH{i},[cRED(i),cGREEN(i),cBLUE(i)]/255);
     chromBW(:,:,:,i) = imoverlay(imgCH{i},maskCH{i},backgroundRGB/255);
     achrom(:,:,:,i) = imoverlay(imgACH{i},maskACH{i},backgroundRGB/255);
     circle = zeros(size(imgCircle));
@@ -49,7 +49,7 @@ for i = 1:imageNumbers
     set(gca,'LooseInset',get(gca,'TightInset'));
     exportgraphics(gca, [num2str(i) 'circle.png'], 'BackgroundColor','none')
     circle_import = imread([num2str(i) 'circle.png']);
-    final = circle_import(9:end-9,9:end-9,:);
+    final = circle_import(9:end-8,9:end-8,:);
     colorcircles(:,:,:,i) = final;
 end
 
